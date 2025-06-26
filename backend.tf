4e12e595e584f1645ad2b0d3ab9ff401fb9163bd
Shuchita Chiwande <145900918+shuchita1234@users.noreply.github.com>
2025-06-26 16:59:46 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "azurerm" {
     resource_group_name   = "WisdomPetMedicine-RG"
     storage_account_name  = "storageaccount30808"
-    container_name        = "marketing"
-    key                   = "terraform.tfstate"
+    container_name        = "netcoreapp"
+    key                   = "dev.tfstate"
   }
 }
